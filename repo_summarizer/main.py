1913f6374e868e754f68a81308a86fbf0090d5a2
Martin Hinrichs <mail@martinhinrichs.net>
2023-04-20 13:53:06 +0200

    Add basic code summarization function

--- repo_summarizer/main.py
+++ repo_summarizer/main.py
@@ -1,3 +1,37 @@
 from dotenv import load_dotenv
-
 load_dotenv()
+
+from langchain.text_splitter import PythonCodeTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.summarize import load_summarize_chain
+import tiktoken
+
+
+def summarize(filepath: str) -> str:
+    def count_tokens(text):
+        enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        encoded = enc.encode(text)
+        return len(encoded)
+
+    """Summarize a single file."""
+    with open(filepath, "r") as f:
+        try:
+            text = f.read()
+        except UnicodeDecodeError:
+            return ""
+
+    if text.strip() == "":
+        return ""
+
+    splitter = PythonCodeTextSplitter(chunk_size=3500, chunk_overlap=0, length_function=count_tokens)
+
+    docs = splitter.create_documents([text])
+
+    llm = ChatOpenAI()
+
+    chain = load_summarize_chain(llm, chain_type="refine", verbose=True)
+
+    summary = chain.run(docs)
+    return summary
+
+print(summarize('main.py'))
