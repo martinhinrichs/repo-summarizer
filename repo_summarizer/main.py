de193caa0a02329389242e38c75dd3dd046730ae
Martin Hinrichs <mail@martinhinrichs.net>
2023-04-21 13:31:20 +0200

    Introduce additional functions

--- repo_summarizer/main.py
+++ repo_summarizer/main.py
@@ -6,7 +6,7 @@ import json
 import os
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
-from typing import Dict
+from typing import Dict, Set, List
 from collections import OrderedDict
 import click
 
@@ -23,31 +23,32 @@ def main(path):
         print(summarize(path))
 
 
-def summarize_repository(path: str) -> Dict[str, str]:
-    """Summarize all files in a repository."""
-    if not os.path.exists(os.path.join(path, ".git")):
-        raise ValueError("The path is not a git repository.")
+def list_files(path: str) -> Set[str]:
+    def exclude_files(tracked_files: Set[str], path: str) -> Set[str]:
+        if os.path.exists(os.path.join(path, "repo-summarizer.json")):
+            with open(os.path.join(path, "repo-summarizer.json"), "r") as f:
+                config = json.load(f)
+            for file in config.get("exclude", []):
+                if file in tracked_files:
+                    tracked_files.remove(file)
+
+        return tracked_files
+
     tracked_files_output = subprocess.check_output(["git", "ls-files"], cwd=path, universal_newlines=True)
     tracked_files = set(tracked_files_output.strip().splitlines())
+    return exclude_files(tracked_files, path)
 
-    # Exclude files listed in repo-summarizer.json
-    if os.path.exists(os.path.join(path, "repo-summarizer.json")):
-        with open(os.path.join(path, "repo-summarizer.json"), "r") as f:
-            config = json.load(f)
-        for file in config.get("exclude", []):
-            if file in tracked_files:
-                tracked_files.remove(file)
 
-    # Ask for confirmation if summarization price is > 0.1 USD
-    tokens = {file: count_tokens_in_file(os.path.join(path, file)) for file in tracked_files}
-    tokens = OrderedDict(sorted(tokens.items()))
+def calculate_cost_and_confirm(tracked_files: Set[str], repo_path: str) -> None:
+    tracked_files_full_path = [os.path.join(repo_path, file) for file in tracked_files]
 
+    tokens = {file: count_tokens_in_file(file) for file in tracked_files}
     total_tokens = sum(tokens.values())
-    price = 0.002 # price per 1k tokens for chatgpt-3.5-turbo
+    price = 0.002  # price per 1k tokens for chatgpt-3.5-turbo
     cost = (total_tokens / 1000) * price
     limit = 0.0
 
-    if (cost > limit):
+    if cost > limit:
         click.echo(f"Total tokens: {total_tokens}")
         files_by_size_desc = OrderedDict(sorted(tokens.items(), key=lambda x: x[1], reverse=True))
 
@@ -56,8 +57,18 @@ def summarize_repository(path: str) -> Dict[str, str]:
             click.echo(f"  {file}: {size}")
 
         click.echo()
-        if click.confirm(f"Warning: The cost of summarizing this repository is {cost} USD ({price} USD/1k tokens). Do you want to continue?") is False:
+        if click.confirm(
+                f"Warning: The cost of summarizing this repository is {cost} USD ({price} USD/1k tokens). Do you want to continue?") is False:
             raise click.Abort()
+
+
+def summarize_repository(path: str) -> Dict[str, str]:
+    """Summarize all files in a repository."""
+    if not os.path.exists(os.path.join(path, ".git")):
+        raise ValueError("The path is not a git repository.")
+
+    tracked_files = list_files(path)
+    calculate_cost_and_confirm(tracked_files, path)
 
     with ThreadPoolExecutor(max_workers=20) as executor:
         file_summaries = list(executor.map(summarize, tracked_files))
