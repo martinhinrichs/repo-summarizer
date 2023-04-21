7b88c252c29505e9c2aa1995544921ce8dd7a640
Martin Hinrichs <mail@martinhinrichs.net>
2023-04-21 13:12:16 +0200

    Ask for confirmation if API usage costs exceed a limit

--- repo_summarizer/main.py
+++ repo_summarizer/main.py
@@ -11,6 +11,7 @@ from collections import OrderedDict
 import click
 
 from repo_summarizer.summarize import summarize
+from repo_summarizer.tokens import count_tokens_in_file
 
 
 @click.command()
@@ -29,12 +30,34 @@ def summarize_repository(path: str) -> Dict[str, str]:
     tracked_files_output = subprocess.check_output(["git", "ls-files"], cwd=path, universal_newlines=True)
     tracked_files = set(tracked_files_output.strip().splitlines())
 
+    # Exclude files listed in repo-summarizer.json
     if os.path.exists(os.path.join(path, "repo-summarizer.json")):
         with open(os.path.join(path, "repo-summarizer.json"), "r") as f:
             config = json.load(f)
         for file in config.get("exclude", []):
             if file in tracked_files:
                 tracked_files.remove(file)
+
+    # Ask for confirmation if summarization price is > 0.1 USD
+    tokens = {file: count_tokens_in_file(os.path.join(path, file)) for file in tracked_files}
+    tokens = OrderedDict(sorted(tokens.items()))
+
+    total_tokens = sum(tokens.values())
+    price = 0.002 # price per 1k tokens for chatgpt-3.5-turbo
+    cost = (total_tokens / 1000) * price
+    limit = 0.0
+
+    if (cost > limit):
+        click.echo(f"Total tokens: {total_tokens}")
+        files_by_size_desc = OrderedDict(sorted(tokens.items(), key=lambda x: x[1], reverse=True))
+
+        click.echo(f"Top 10 files by tokens:")
+        for file, size in list(files_by_size_desc.items())[:10]:
+            click.echo(f"  {file}: {size}")
+
+        click.echo()
+        if click.confirm(f"Warning: The cost of summarizing this repository is {cost} USD ({price} USD/1k tokens). Do you want to continue?") is False:
+            raise click.Abort()
 
     with ThreadPoolExecutor(max_workers=20) as executor:
         file_summaries = list(executor.map(summarize, tracked_files))
--- repo_summarizer/tokens.py
+++ repo_summarizer/tokens.py
@@ -5,3 +5,17 @@ def count_tokens(text):
     enc = tiktoken.encoding_for_model("gpt-3.5-turbo")
     encoded = enc.encode(text)
     return len(encoded)
+
+
+def count_tokens_in_file(path: str) -> int:
+    """Count the number of tokens in a file."""
+    with open(path, "r") as f:
+        try:
+            text = f.read()
+        except UnicodeDecodeError:
+            return 0
+
+    if text.strip() == "":
+        return 0
+
+    return count_tokens(text)
